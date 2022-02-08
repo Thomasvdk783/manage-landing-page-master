d8039d193b9c84ad8cd58bd05db3c2bbb0a3e8fe
Thomasvdk783 <thomas.vandekerchove@outlook.com>
2022-02-08 20:16:46 +0100

    mise en place du javascript pour le menu en version mobile(encore à terminer)

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
 });
+
+
+
+/// nav mobile ///
+const navMobile = document.querySelector('.nav-header-mobile');
+const btnOpenNavMobile = document.querySelector('.fa-bars');
+const icons = document.querySelector('i')
+
+icons.addEventListener("click", () => {
+    navMobile.style.display = "block"
+    icons.classList.toggle('fa-times');
+});
+
+
